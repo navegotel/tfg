3de9f3533fdaa73a2655aa5b033ea5899f5b4740
markus barth <markus@markusbarth.net>
2025-03-02 21:02:03 +0100

    adding authentication

--- tfgcore/urls.py
+++ tfgcore/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path('question04', views.question04, name='question04'),
     path('question05', views.question05, name='question05'),
     path('question06', views.question06, name='question06'),
+    path('question07', views.question07, name='question07'),
+    path('question08', views.question08, name='question08'),
+    path('question09', views.question09, name='question09'),
+    path('bye', views.bye, name='bye'),
     path('legalstuff', views.legalstuff, name='legalstuff'),
     path('testformwidgets', views.testformwidgets, name='testformwidgets'),
 ]
--- tfgcore/views.py
+++ tfgcore/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.urls import reverse
 from .models import Participant
 
@@ -15,7 +18,6 @@ def about(request):
 def personaldata(request):
     if request.method == "POST":
         msgs = []
-        #TODO first check if the user is logged in
         try:
             if len(request.POST["email"]) == 0:
                 msgs.append("Tienes que especificar un correo electrónico.")
@@ -31,13 +33,17 @@ def personaldata(request):
         h=hash(request.POST["email"])
         participants = Participant.objects.filter(hash=h)
         if len(participants) == 0:
-            #TODO create user
-            pass
+            username = str(abs(h))
+            email = "{0}@markusbarth.net".format(username)
+            password = "unsecureP@sswrd"
+            User.objects.create_user(username, email, password)
+            usr = authenticate(username=username, password=password)
+            
         return redirect(reverse('tfgcore:question01'))
         
     return render(request, 'tfgcore/personaldata.html')
 
-
+@login_required
 def question01(request):
     try:
         if request.POST["intreatment"] in ['2', '3']:
@@ -72,11 +78,35 @@ def question05(request):
         return redirect(reverse('tfgcore:question06'))
     return render(request, 'tfgcore/question05.html')
 
+
 def question06(request):
     if request.method == "POST":
         return redirect(reverse('tfgcore:question07'))
     return render(request, 'tfgcore/question06.html')
 
+
+def question07(request):
+    if request.method == "POST":
+        return redirect(reverse('tfgcore:question08'))
+    return render(request, 'tfgcore/question07.html')
+
+
+def question08(request):
+    if request.method == "POST":
+        return redirect(reverse('tfgcore:question09'))
+    return render(request, 'tfgcore/question08.html')
+
+
+def question09(request):
+    if request.method == "POST":
+        return redirect(reverse('tfgcore:bye.html'))
+    return render(request, 'tfgcore/question09.html')
+
+
+def bye(request):
+    if request.method == "POST":
+        return redirect(reverse('tfgcore:bye'))
+    return render(request, 'tfgcore/bye.html')
 
 def legalstuff(request):
     return render(request, 'tfgcore/aviso_legal.html')
